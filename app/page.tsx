e148c38f9c07a8a36e062a6f856ccd24bc76df2d
unknown <saadsyed950@gmail.com>
2025-01-24 20:32:19 +0530

    Clerk auth is working perfectly

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
+"use client";
+
+import { SignInButton, useUser } from "@clerk/nextjs";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const { user } = useUser();
+  return (
+    <div>
+      <SignInButton />
+      <h1>{user?.firstName}</h1>
+    </div>
+  );
 };
 
 export default HomePage;
