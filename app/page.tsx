5d6422920ede21606e1a1dfca66a0f8cec142108
unknown <saadsyed950@gmail.com>
2025-01-28 20:02:09 +0530

    Finally it's working

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,40 @@
 "use client";
 
-import { SignInButton, useUser } from "@clerk/nextjs";
+interface User {
+  id: string;
+  firstName: string | null;
+  lastName: string | null;
+  emailAddresses: { emailAddress: string }[];
+}
+
+import { useUser } from "@clerk/nextjs";
+import axios from "axios";
+import { useEffect } from "react";
 
 const HomePage = () => {
   const { user } = useUser();
-  return (
-    <div>
-      <SignInButton />
-      <h1>{user?.firstName}</h1>
-    </div>
-  );
+
+  useEffect(() => {
+    const saveUser = async (user: User) => {
+      if (!user) return;
+      try {
+        await axios.post("http://localhost:8080/api/user", {
+          userid: user.id,
+          firstname: user.firstName ?? "",
+          lastname: user.lastName ?? "",
+          email: user.emailAddresses[0]?.emailAddress ?? "",
+        });
+      } catch (error) {
+        console.error("Error saving user to DB", error);
+      }
+    };
+
+    if (user) {
+      saveUser(user);
+    }
+  });
+
+  return <div></div>;
 };
 
 export default HomePage;
