06fc1cdfc3e28828513fc7c6f8166499a472a3c5
unknown <saadsyed950@gmail.com>
2025-01-28 20:12:29 +0530

    Working on Navbar

---
+++ app/_components/Navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <div className="flex justify-between items-center bg-white text-black w-full lg:px-10 lg:py-4">
+      Navbar
+    </div>
+  );
+};
+
+export default Navbar;
